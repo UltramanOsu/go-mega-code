1317c3ac17acdb114d13d421353f1b40e15abb8f
Tao Chen <qq1026264112@outlook.com>
2022-02-18 10:49:38 +0800

    hellowork working now

--- main.go
+++ main.go
@@ -5,6 +5,6 @@ import "net/http"
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello World"))
-		http.ListenAndServe(":8888", nil)
 	})
+	http.ListenAndServe(":8888", nil)
 }
